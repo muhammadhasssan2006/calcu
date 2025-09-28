2b0ad49784aeb8f785289e7c00c89483a9e5ddb6
muhammadhasssan2006 <hassanweb365@gmail.com>
2025-09-28 17:29:21 +0500

    calculator completed

---
+++ app.js
@@ -0,0 +1,34 @@
+var input = document.getElementById("inputvalue");
+
+function getValue(value){
+    input.value = input.value + value;
+}
+
+function square(){
+input.value = input.value * input.value;
+
+}
+function clrAll(){
+    input.value = "";
+}
+function clr(){
+    input.value = input.value.slice(0,-1);
+}
+function equal() {
+  input.value = eval(input.value);
+}
+
+
+
+function getoperator(operator) {
+    let lastChar = input.value.slice(-1); 
+
+    if (input.value === "") return;
+
+    if (['+', '-', '*', '/','%'].includes(lastChar)) {
+        input.value = input.value.slice(0, -1) + operator;
+    } 
+    else {
+        input.value += operator;
+    }
+}
